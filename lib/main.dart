52cfb8d91cbeba2cec8e292ee58557cba16be58f
EvertonFi <jose.everton.f@hotmail.com>
2020-03-11 19:20:56 -0300

    Correção do bug de carregamento da api

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Api> _api;
+  List<Api> _api = List(); //Adicionar uma lista já vazia a variavel.
+  bool visual = false; //Cria uma variavel para ajudar a so mostrar quando terminar o carregamento da requisição
 
   Future<List<Api>> _getUser() async {
     try {
@@ -47,8 +48,10 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     _getUser().then((map) {
-      _api = map;
-      print(_api.length);
+      setState(() {//muda o status da variavel 
+        _api = map;
+        visual = true; //muda a visualização para verdadeira
+      });
     });
   }
 
@@ -79,149 +82,158 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Container(
-                height: 50,
-                alignment: Alignment.bottomCenter,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.redAccent,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10),
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40),
-                      ),
-                      color: Colors.white),
-                  height: MediaQuery.of(context).size.height - 200,
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(top: 15, left: 40, right: 40),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text("Status"),
-                            IconButton(
-                              icon: Icon(Icons.more_vert),
-                              onPressed: () {},
-                            )
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 20, right: 20),
-                        child: Container(
-                          height: 60,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: _api.length,
-                            itemBuilder: (BuildContext contexte, int index) {
-                              return Container(
-                                child: CircleAvatar(
-                                  radius: 35,
-                                  backgroundColor: Colors.redAccent,
-                                  child: CircleAvatar(
-                                    backgroundColor: Colors.white,
-                                    radius: 28,
-                                    backgroundImage: NetworkImage(
-                                        "https://loremflickr.com/320/32$index"),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 150, right: 10, bottom: 10),
-                child: Container(
-                  height: MediaQuery.of(context).size.height - 240,
-                  child: ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          visual == false //quando a visualização for True (Verdadeiro) muda do conteiner par o stack
+              ? Container()
+              : Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 50,
+                      alignment: Alignment.bottomCenter,
+                      width: MediaQuery.of(context).size.width,
+                      color: Colors.redAccent,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(40),
+                              topRight: Radius.circular(40),
+                            ),
+                            color: Colors.white),
+                        height: MediaQuery.of(context).size.height - 200,
+                        child: Column(
                           children: <Widget>[
                             Padding(
-                              padding: EdgeInsets.all(8),
-                              child: Container(
-                                child: CircleAvatar(
-                                  radius: 30,
-                                  backgroundColor: Colors.white,
-                                  backgroundImage: NetworkImage(
-                                      "https://loremflickr.com/320/32$index"),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: MediaQuery.of(context).size.width - 160,
-                              child: Column(
+                              padding: EdgeInsets.only(
+                                  top: 15, left: 40, right: 40),
+                              child: Row(
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: <Widget>[
-                                  Text(
-                                    _api[index].name,
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    _api[index].address.street,
-                                    style: TextStyle(fontSize: 17),
-                                    overflow: TextOverflow.ellipsis,
+                                  Text("Status"),
+                                  IconButton(
+                                    icon: Icon(Icons.more_vert),
+                                    onPressed: () {},
                                   )
                                 ],
                               ),
                             ),
                             Padding(
-                              padding: EdgeInsets.only(right: 10),
-                              child: Column(
+                              padding: EdgeInsets.only(left: 20, right: 20),
+                              child: Container(
+                                height: 60,
+                                child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: _api.length,
+                                  itemBuilder:
+                                      (BuildContext contexte, int index) {
+                                    return Container(
+                                      child: CircleAvatar(
+                                        radius: 35,
+                                        backgroundColor: Colors.redAccent,
+                                        child: CircleAvatar(
+                                          backgroundColor: Colors.white,
+                                          radius: 28,
+                                          backgroundImage: NetworkImage(
+                                              "https://loremflickr.com/320/32$index"),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.only(top: 150, right: 10, bottom: 10),
+                      child: Container(
+                        height: MediaQuery.of(context).size.height - 240,
+                        child: ListView.builder(
+                            itemCount: 10,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Row(
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceBetween,
                                 children: <Widget>[
-                                  Text("17:00"),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Container(
-                                    height: 30,
-                                    decoration: BoxDecoration(
-                                        color: index != 0
-                                            ? Colors.red
-                                            : Colors.white,
-                                        borderRadius:
-                                            BorderRadius.circular(20)),
-                                    child: Padding(
-                                      padding: EdgeInsets.all(8),
-                                      child: Text(
-                                        index != 0 ? '$index' : ' ',
-                                        style: TextStyle(color: Colors.white),
+                                  Padding(
+                                    padding: EdgeInsets.all(8),
+                                    child: Container(
+                                      child: CircleAvatar(
+                                        radius: 30,
+                                        backgroundColor: Colors.white,
+                                        backgroundImage: NetworkImage(
+                                            "https://loremflickr.com/320/32$index"),
                                       ),
                                     ),
+                                  ),
+                                  Container(
+                                    width: MediaQuery.of(context).size.width -
+                                        160,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(
+                                          _api[index].name,
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          _api[index].address.street,
+                                          style: TextStyle(fontSize: 17),
+                                          overflow: TextOverflow.ellipsis,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(right: 10),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Text("17:00"),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Container(
+                                          height: 30,
+                                          decoration: BoxDecoration(
+                                              color: index != 0
+                                                  ? Colors.red
+                                                  : Colors.white,
+                                              borderRadius:
+                                                  BorderRadius.circular(20)),
+                                          child: Padding(
+                                            padding: EdgeInsets.all(8),
+                                            child: Text(
+                                              index != 0 ? '$index' : ' ',
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
                                   )
                                 ],
-                              ),
-                            )
-                          ],
-                        );
-                      }),
+                              );
+                            }),
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
-          ),
         ],
       ),
     );
   }
 }
-
